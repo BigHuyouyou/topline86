ebb0622d21321d6f5d4a3a6970dafa31582367cd
ymy <1197561126@qq.com>
2019-11-09 22:38:07 +0800

    feat:完成验证

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 
 import '@/assets/css/global.css'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,19 @@
   <div class="login-container">
     <div class="login-box">
       <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules">
-        <img src="./logo_index.png" alt="">
+        <img src="./logo_index.png" alt />
         <el-form-item prop="mobile">
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号码"></el-input>
         </el-form-item>
         <el-form-item prop="code">
           <el-input v-model="loginForm.code" placeholder="请输入校验码"></el-input>
         </el-form-item>
-        <el-form-item style="text-align:left;">
+        <el-form-item style="text-align:left;" prop="xieyi">
           <el-checkbox v-model="loginForm.xieyi" style="margin-right:10px"></el-checkbox>
           <span>我已阅读并同意用户协议和隐私条款</span>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%;" type="primary"  @click="login">登录</el-button>
+          <el-button style="width:100%;" type="primary" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -24,6 +24,18 @@
 <script>
 export default {
   data () {
+    var xieyiTest = function (rule, value, callback) {
+      // rule:校验规则，一般不用
+      // value:当前被校验的信息
+      // callback：回调函数，校验成功或失败都需要执行
+      // if (value) {
+      //   callback()
+      // } else {
+      //   return callback(new Error('请无条件遵守规矩'))
+      // }
+
+      value ? callback() : callback(new Error('请无条件遵守规矩'))
+    }
     return {
       loginForm: {
         mobile: '',
@@ -36,20 +48,32 @@ export default {
           { required: true, message: '手机号必填' },
           { pattern: /^1[35789]\d{9}$/, message: '手机号码格式不对' }
         ],
-        code: [
-          { required: true, message: '验证码必填' }
-        ]
-
+        code: [{ required: true, message: '验证码必填' }],
+        xieyi: [{ validator: xieyiTest }]
       }
-
     }
   },
   methods: {
     login () {
       this.$refs.loginFormRef.validate(valid => {
-        if (valid) {
-          this.$router.push('/home')
+        if (!valid) {
+          //
+          return false
         }
+        var pro = this.$http.post('/authorizations', this.loginForm)
+        pro
+          .then(result => {
+            if (result.data.message === 'OK') {
+              window.sessionStorage.setItem(
+                'userinfo',
+                JSON.stringify(result.data.data)
+              )
+              this.$router.push('/home')
+            }
+          })
+          .catch(err => {
+            return this.$message.error('用户名或密码错误' + err)
+          })
       })
     }
   }
@@ -73,11 +97,11 @@ export default {
     justify-content: center;
     align-items: center;
     text-align: center;
-    img{
+    img {
       width: 50%;
       margin: 20px auto;
     }
-    .el-form{
+    .el-form {
       width: 75%;
     }
   }
